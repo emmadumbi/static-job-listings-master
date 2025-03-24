5354fda521ecb0e88212111528faeb7ac7937b43
emmadumbi <dumbiemmanuel29@gmail.com>
2025-03-24 13:34:21 +0100

    ok

--- script.js
+++ script.js
@@ -1,73 +1,101 @@
 "use strict";
 
-const section = document.querySelector("section");
+const filterContainer = document.querySelector(".filter-container");
+const filter = document.querySelector("#filter");
+const clearBtn = document.querySelector("#clear");
+const joblistings = document.querySelector("#job-listings");
 
-async function getData() {
+let selectedFilters = [];
+
+(async function () {
   const res = await fetch("./data.json");
   const data = await res.json();
   data.forEach((job) => {
-    const container = document.createElement("div");
-    container.innerHTML = `
-      <img src="${job.logo}" alt="company-logo"/>
-      <div class="head">
-        <div class="wrapper">
-          <div class="top">
-            <h2>${job.company}</h2>
-            <div class="stat"></div>
-          </div>
-          <a href="#">${job.position}</a>
-          <ul class="availability">
-            <li>${job.postedAt}</li>
-            <li>${job.contract}</li>
-            <li>${job.location}</li>
-          </ul>  
-        </div>
-        <hr/>
-        <div class="tags"></div>
-      </div>    
+    const jobCard = document.createElement("div");
+    jobCard.classList.add("job-card");
+    jobCard.innerHTML = `
+      <img src="${job.logo}" alt="company logo"/>
+      <div class="heading">
+        <h1>${job.company}</h1>
+        <div class="stat"></div>
+      </div>
+      <p>${job.position}</p>
+      <span>${job.postedAt}</span>
+      <span>${job.contract}</span>
+      <span>${job.location}</span>
+      <hr>
+      <div class="tags"></div>
+
     `;
-
-    if (job.new) {
-      const span = document.createElement("span");
-      span.classList.add("new");
-      span.textContent = "New!";
-      container.querySelector(".stat").append(span);
-    }
-
-    if (job.featured) {
-      const span = document.createElement("span");
-      span.classList.add("feature");
-      span.textContent = "Featured";
-      container.querySelector(".stat").append(span);
-    }
 
     const types = job.position.split(" ");
 
-    const btn1 = document.createElement("button");
-    btn1.dataset.type = job.role;
-    btn1.textContent = job.role;
-    container.querySelector(".tags").appendChild(btn1);
+    const roleBtn = document.createElement("button");
+    roleBtn.textContent = job.role;
+    jobCard.querySelector(".tags").appendChild(roleBtn);
 
-    const btn2 = document.createElement("button");
-    btn2.dataset.type = job.level;
-    btn2.textContent = job.level;
-    container.querySelector(".tags").appendChild(btn2);
+    const levelBtn = document.createElement("button");
+    levelBtn.textContent = job.level;
+    jobCard.querySelector(".tags").appendChild(levelBtn);
 
     job.languages.forEach((lang) => {
-      const btn = document.createElement("button");
-      btn.dataset.type = lang;
-      btn.textContent = lang;
-      container.querySelector(".tags").appendChild(btn);
+      const langbtn = document.createElement("button");
+      langbtn.textContent = lang;
+      jobCard.querySelector(".tags").appendChild(langbtn);
     });
 
     job.tools.forEach((tool) => {
-      const btn = document.createElement("button");
-      btn.dataset.type = tool;
-      btn.textContent = tool;
-      container.querySelector(".tags").appendChild(btn);
+      const toolbtn = document.createElement("button");
+      toolbtn.textContent = tool;
+      jobCard.querySelector(".tags").appendChild(toolbtn);
     });
 
-    section.appendChild(container);
+    if (job.new) {
+      const newJob = document.createElement("span");
+      newJob.textContent = "New!";
+      newJob.classList.add("new-Job");
+      jobCard.querySelector(".stat").appendChild(newJob);
+    }
+
+    if (job.featured) {
+      const featuredJob = document.createElement("span");
+      featuredJob.textContent = "Featured";
+      featuredJob.classList.add("featured-Job");
+      jobCard.querySelector(".stat").appendChild(featuredJob);
+    }
+
+    joblistings.appendChild(jobCard);
+  });
+  addFilter();
+})();
+
+function addFilter() {
+  document.querySelectorAll(".tags button").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (!selectedFilters.includes(btn.textContent)) {
+        selectedFilters.push(btn.textContent);
+        const filterTag = document.createElement("button");
+        filterTag.textContent = btn.textContent;
+
+        const removeBtn = document.createElement("button");
+        removeBtn.innerHTML = `<img src="images/icon-remove.svg" alt="remove tag button"/>`;
+        removeBtn.addEventListener("click", () => {
+          removeFilterTag(btn.textContent, filterTag);
+        });
+
+        filterTag.appendChild(removeBtn);
+        filter.appendChild(filterTag);
+        filterContainer.style.display = "flex";
+      }
+    });
   });
 }
-getData();
+
+function removeFilterTag(tag, filterTagElement) {
+  selectedFilters = selectedFilters.filter((item) => item !== tag);
+  filterTagElement.remove();
+
+  if (selectedFilters.length === 0) {
+    filterContainer.style.display = "none";
+  }
+}
--- style-guide.md
+++ style-guide.md
@@ -300,3 +300,171 @@ React
 Sass
 
   <!-- Item End -->
+
+"use strict";
+
+const main = document.querySelector("main");
+const filter = document.querySelector(".filter-sub");
+const clear = document.querySelector(".clear");
+
+let closes = [];
+let arr = [];
+
+(async function () {
+const res = await fetch("./data.json");
+const data = await res.json();
+data.forEach((job) => {
+const container = document.createElement("div");
+container.innerHTML = `      <img src="${job.logo}" alt="company-logo"/>
+      <div class="head">
+        <div class="wrapper">
+          <div class="top">
+            <h2>${job.company}</h2>
+            <div class="stat"></div>
+          </div>
+          <a href="#">${job.position}</a>
+          <ul class="availability">
+            <li>${job.postedAt}</li>
+            <li>${job.contract}</li>
+            <li>${job.location}</li>
+          </ul>  
+        </div>
+        <hr/>
+        <div class="tags"></div>
+      </div>    
+   `;
+
+    if (job.new) {
+      const span = document.createElement("span");
+      span.classList.add("new");
+      span.textContent = "New!";
+      container.querySelector(".stat").append(span);
+    }
+
+    if (job.featured) {
+      const span = document.createElement("span");
+      span.classList.add("feature");
+      span.textContent = "Featured";
+      container.querySelector(".stat").append(span);
+    }
+
+    const types = job.position.split(" ");
+
+    const btn1 = document.createElement("button");
+    btn1.dataset.type = job.role;
+    btn1.textContent = job.role;
+    container.querySelector(".tags").appendChild(btn1);
+
+    const btn2 = document.createElement("button");
+    btn2.dataset.type = job.level;
+    btn2.textContent = job.level;
+    container.querySelector(".tags").appendChild(btn2);
+
+    job.languages.forEach((lang) => {
+      const btn = document.createElement("button");
+      btn.dataset.type = lang;
+      btn.textContent = lang;
+      container.querySelector(".tags").appendChild(btn);
+    });
+
+    job.tools.forEach((tool) => {
+      const btn = document.createElement("button");
+      btn.dataset.type = tool;
+      btn.textContent = tool;
+      container.querySelector(".tags").appendChild(btn);
+    });
+
+    main.appendChild(container);
+
+});
+
+const buttons = main.querySelectorAll(".tags button");
+buttons.forEach((button) => {
+button.addEventListener("click", () => {
+filterType(button.dataset.type);
+updateContainer();
+});
+});
+})();
+
+clear.addEventListener("click", () => {
+arr = [];
+filter.innerHTML = "";
+main
+.querySelectorAll(".container")
+.forEach((container) => container.classList.remove("remove"));
+filter.closest(".filter").styledisplay = "none";
+});
+
+function addFilter() {
+filter.innerHTML = "";
+arr.forEach((element) => {
+const El = document.createElement("div");
+El.classList.add("span");
+El.innerHTML = `      <p>${element}</p>
+      <button aria-label="remove button">
+        <img src="imgaes/icon-remove.svg" alt="remove btn"/>
+      </button>
+   `;
+filter.appendChild(El);
+filter.closest(".filter").style.display = "flex";
+
+    closes = [];
+    closes.push(El.querySelector("button"));
+    closes.forEach((close) => {
+      close.addEventListener("click", () => {
+        filter.removeChild(close.closest(".span"));
+        arr.splice(arr.indexOf(close.previousElementSibling.textContent), 1);
+        updateContainer();
+        if (filter.innerHTML) {
+          filter.closest(".filter").style.display = "flex";
+        } else {
+          filter.closest(".filter").style.display = "none";
+        }
+      });
+    });
+
+});
+}
+
+function filterType(type) {
+if (!arr.includes(type)) {
+arr.push(type);
+addFilter();
+}
+}
+
+function updateContainer() {
+const containers = main.querySelectorAll("container");
+containers.forEach((container) => {
+const buttons = container.querySelector("button");
+let check = [];
+buttons.forEach((button) => {
+check.push(button.dataset.type);
+});
+});
+
+let include = true;
+arr.forEach((element) => {
+if (!check.includes(element)) {
+include = false;
+}
+});
+
+if (!include) {
+container.classList.add("remove");
+} else {
+container.classList.remove("remove");
+}
+}
+
+function changeBg() {
+if (document.body.clientWidth < 700) {
+Headers.style["background-image"] = "url(images/bg-header-mobile.svg)";
+} else {
+Headers.style["background-image"] = "url(images/bg-header-desktop.svg)";
+}
+}
+
+changeBg();
+window.onresize = changeBg;
